2b5f9d475873f867990e222f772b476bea269210
Dmytro-Tarasenko <ug.dotenko@gmail.com>
2024-03-07 11:02:08 +0200

    Update, requierements.txt

--- cli_crud.py
+++ cli_crud.py
@@ -1,5 +1,34 @@
 """CLI nightmare for CRUD db operations"""
 import argparse
+from typing import Iterable, Any
 
+from sqlalchemy import create_engine, func, desc, and_
+from sqlalchemy.orm import sessionmaker
+from collections import namedtuple
 
 from orms import Student, Subject, Tutor, Group, Mark
+
+
+def make_ntuple(name: str,
+                fields: list[str],
+                values: Iterable) -> Any:
+    cls = namedtuple(name, fields)
+    return cls(*values)
+
+
+def create():
+    pass
+
+
+def read():
+    pass
+
+
+def update():
+    pass
+
+
+def delete():
+    pass
+
+
